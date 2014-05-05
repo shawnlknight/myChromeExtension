51b24e72ae4fc6fe924efc3d8360dc7d46f3a2d7
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-05-05 07:11:56 -0400

    first commit

---
+++ app/scripts/popup.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+
+// $.get( "http://www.reddit.com/.json", function( data ) {
+//   $( ".result" ).html( data );
+//   alert( "Load was performed." );
+// });
+
+(function() {
+  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  $.getJSON( flickerAPI, {
+    tags: "mount rainier",
+    tagmode: "any",
+    format: "json"
+  })
+    .done(function( data ) {
+      $.each( data.items, function( i, item ) {
+        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+        if ( i === 3 ) {
+          return false;
+        }
+      });
+    });
+})();
+
+});
