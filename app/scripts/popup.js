89b810c4e892ced6631538fa565020ac2f865402
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-05-06 10:02:47 -0400

    news subreddit

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,29 +1,31 @@
 $(document).ready(function(){
 
 
-$.getJSON("http://www.reddit.com/.json", function(json) {
-  var post = json.data.children;
-  var reddit = '';
-
-   for(var i=0, l=post.length; i<l; i++) {
-        var object = post[i].data;
-
-        if(object.thumbnail === 'default' || object.thumbnail === 'nsfw' || object.thumbnail === 'self' || object.thumbnail === '')
-          object.thumbnail = 'images/steve.jpg';
 
 
-        reddit += '<img src="'+object.thumbnail+'" class="thumbImg">\n';
-        reddit += '<div class="linkDetails"><a href="'+object.url+'" target="_blank"><div class="title">'+object.title+'</div></a>\n';
+  $.getJSON("http://www.reddit.com/r/news.json", function(json) {
+    var post = json.data.children;
+    var reddit = '';
 
-      }
+     for(var i=0, l=post.length; i<l; i++) {
+          var object = post[i].data;
+
+          if(object.thumbnail === 'default' || object.thumbnail === 'nsfw' || object.thumbnail === 'self' || object.thumbnail === '')
+            object.thumbnail = 'images/steve.jpg';
 
 
-      // Insert into DOM
-      $(".posts").html(reddit);
+          reddit += '<img src="'+object.thumbnail+'" class="thumbImg">\n';
+          reddit += '<div class="linkDetails"><a href="'+object.url+'" target="_blank"><div class="title">'+object.title+'</div></a>\n';
+
+        }
+
+
+        // Insert into DOM
+        $(".posts").html(reddit);
 
 
 
-});
+  });
 
 
 });
